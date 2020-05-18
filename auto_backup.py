706d8bc0255a3118670869da7522e2f64209e294
vdf13 <vdf13@free.fr>
2020-05-18 15:51:29 +0200

    Version finale avant merge sur master

--- auto_backup.py
+++ auto_backup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3.7
 # -*- coding: utf-8 -*-
 
-# Victor De Faria 2020-04-27 2020-05-xx
+# Victor De Faria 2020-04-27 2020-05-18
 # Algorythme du programme
 #   |
 #   |-> Générer le nom de la sauvevarde  <- Source fichier data.yml
@@ -16,7 +16,11 @@ import os, sys, re, datetime, glob, netrc, ftplib, yaml, tarfile
 # DEFINITION DES FONCTIONS
 
 def nom(texte, base_name):
-    ''' Fonction qui génère un nom de fichier en fonction de la date et heure de la sauvegarde '''
+    '''
+    Fonction qui génère un nom de fichier en fonction de
+    la date et heure de la sauvegarde
+    un nom donné en variable pour différencier les serveurs
+    '''
     date = datetime.datetime.now()
     texte = f'backup-{base_name}-{texte}-{date.strftime("%Y-%m-%d-%H-%M")}.tar.gz'
     return texte
@@ -25,8 +29,9 @@ def nom(texte, base_name):
 def supp_old_backup(texte, base_name, dir_back):
     '''
     Fonction qui récupère la liste des fichiers du répertoire backup
-    et ne renvoie que ceux dont le nom contient 'backup'- expression régulière.
-    Si 8 fichiers dans le répertoire, alors le plus ancien est supprimé.
+    ne renvoie que ceux dont le nom contient 'backup' + 'nom_spécifié' . Utilise les expressions régulières.
+    Si les fichiers dans le répertoire sont plus ancien que 'delai_retention'  alors ils sont supprimés.
+    Renvoie la liste des fichiers de sauvegarde. Utile en cas de DEBUG
     '''
     fichiers = glob.glob(dir_back + "*")
     fichiers.sort(key=os.path.getmtime)
@@ -42,7 +47,7 @@ def supp_old_backup(texte, base_name, dir_back):
                 backup_files.append(reg.group(0))
                 #print("Récent : ", file)    # DEBUG
             else:
-                print(f"La Sauvegarde de plus de {delai_retention} jours va être supprimé du serveur {wp_host}: ", file)    # DEBUG
+                print(f"La Sauvegarde de plus de {delai_retention} jours va être supprimé du serveur {wp_host}: ", file)    # Message qui sera récupéré par le logger lors de l'execution
                 os.remove(file)
             
     return(backup_files)    
@@ -50,8 +55,9 @@ def supp_old_backup(texte, base_name, dir_back):
 def supp_old_ftp_backup(texte, base_name):
     '''
     Fonction qui récupère la liste des fichiers du répertoire de backup sur le serveur FTP 
-    Supprime le plus ancien fichier de backup sur le serveur FTP
-    Renvoie la liste des fichiers de sauvegarde après suppression
+    ne renvoie que ceux dont le nom contient 'backup' + 'nom_spécifié' . Utilise les expressions régulières.
+    Si les fichiers dans le répertoire sont plus ancien que 'delai_retention'  alors ils sont supprimés.
+    Renvoie la liste des fichiers de sauvegarde. Utile en cas de DEBUG
     '''
     # On récupère la liste des fichiers
     now = datetime.datetime.now()
@@ -106,6 +112,7 @@ nom_backup_base = nom(backup_type[1], base_name)
 os.system(f"mysqldump -h localhost -u {nom_user_base} --databases {base_name} > /tmp/backup.sql")
 
 # Utilisation du module tarfile pour créer l' archive de la base wordpress 
+# Dans un fichier temporaire /tmp/backup.sql qui est ensuite supprimé
 tar = tarfile.open(dir_backup + nom_backup_base, 'w:gz')
 tar.add('/tmp/backup.sql')
 tar.close()
@@ -119,15 +126,19 @@ tar.add(dir_site_apache + virtual_host)
 tar.close()
 
 
-# Récupération de la liste des fichiers  de sauvegarde
+# Récupération de la liste des fichiers  de sauvegarde pour serveur local et FTP
 backup_files = supp_old_backup(backup_type[0], base_name, dir_backup)
 backup_bases = supp_old_backup(backup_type[1], base_name, dir_backup)
 
 # ##### ACTIONS SUR LE SERVEUR FTP ###### #
 
 # Récupération des informations de connexion du serveur ftp contenu dans le fichier protégé .netrc
-netrc = netrc.netrc()
-auth_ftp = netrc.authenticators(ftp_host)
+try:
+    netrc = netrc.netrc()
+    auth_ftp = netrc.authenticators(ftp_host)
+except FileNotFoundError:
+    print('Warning : Fichier d\'identification FTP non présent. L\'accès au serveur impossible')
+    sys.exit(2)
 
 # Partie de connexion au serveur FTP et transfert des fichiers de sauvegarde 
 with ftplib.FTP(host= ftp_host, user=auth_ftp[0], passwd=auth_ftp[2]) as ftp:
@@ -140,13 +151,15 @@ with ftplib.FTP(host= ftp_host, user=auth_ftp[0], passwd=auth_ftp[2]) as ftp:
     except ftplib.all_errors as e:
         print('FTP erreur : ', e)
 
-# Récupération de la liste des fichiers et Suppression du fichier de sauvegarde le plus ancien
+# Récupération de la liste des fichiers et Suppression du fichier de sauvegarde les plus ancien
 ftp_files = supp_old_ftp_backup(backup_type[0], base_name)
 ftp_bases = supp_old_ftp_backup(backup_type[1], base_name)
 
+# Message de fin signalant la réussite de l'opération . Pour être tracé dans les logs
+print(f" {nom_backup_file} a été transféré sur le serveur {ftp_host}")
+print(f" {nom_backup_base} a été transféré sur le serveur {ftp_host}")
 
-
-# Tests des résultats des répertoires   en cas besoins de DEBUGAGE
+# Tests des résultats des répertoires   en cas besoins de DEBUGAGE. Listes obtenus par les fonctions 'supp_old_xx'
 #print("WP FILES :\n", backup_files, len(backup_files))   # DEBUG
 #print("WP BASES :\n", backup_bases, len(backup_bases))   # DEBUG
 
